986682b3256cd637271840a8742a82849e1f1034
JD <hooph00p@users.noreply.github.com>
2016-09-12 10:50:25 -0400

    Now using http.status

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func (app *Application) Run() {
 
 		states, err := app.Map.Contains(latitude, longitude)
 		if err != nil {
-    			c.JSON(404, gin.H{"message": "Point not in a state."})
+    			c.JSON(http.StatusNotFound, gin.H{"message": "Point not in a state."})
 		} else {
 			c.JSON(http.StatusOK, states)
 		}
